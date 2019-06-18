b9a5df8d8441c5ce5d739336ccb7ea8a1caf088f
AkhmedovValikhan <akhmedovvalikhan@gmail.com>
2019-06-18 14:50:26 +0400

    style() remove some comments

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,3 @@ import { App } from './app/App';
 
 const appRoot = document.getElementById('root');
 render(<App />, appRoot);
-
-/**
- * Create hisotyr of recent researches
- * 1. store 5 most recent searches
- * 2. we will store search in memomery
- *
- *
- * 1. we sill store
- */
